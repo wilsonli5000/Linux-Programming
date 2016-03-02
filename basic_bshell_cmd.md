08b9b73ea09828d595a98185b98b523e458f427a
wilsonli5000 <wilsonli5000@gmail.com>
2016-03-02 15:00:11 -0800

    two files

---
+++ basic_bshell_cmd.md
@@ -0,0 +1,1 @@
+read	reads in a string from user input set to a variable. 	read name
---
+++ helloworld.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#For mac os x, we use this header.
+#For normal linux, use #!/usr/bin/bash
+
+#echo: prints the following string or variable value. 
+#echo -n: prints the string in the same line. 
+echo "Hello World!"
+
+#Variable: there is only one vairable type, string. 
+#Declaration: a="cat", no space in between.
+#$ to retrieve the value of the variable.
+#We can use a variable without declaring.
+
+
+
