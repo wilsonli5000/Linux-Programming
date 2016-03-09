84712236e8fb9cddf3d2f5387a406cac99bcef52
wilsonli5000 <wilsonli5000@gmail.com>
2016-03-08 22:15:10 -0800

    Update remote git cmd.

--- git_cmd.sh
+++ git_cmd.sh
@@ -30,8 +30,30 @@ git log				// search the history of the repo, to make the output prettier, use -
 
 git reset --hard HEAD^		// reset the repo to previous versions. HEAD represents teh current version, HEAD^ represents previous version, HEAD^^ represents previous second version, for more previous versions, use HEAD~100.
 
-git resetn --hard <commit ID>	// to reset future versions, type part of the commit ID of the repo. commit ID can be found through git log.
+git reset --hard <commit ID>	// to reset future versions, type part of the commit ID of the repo. commit ID can be found through git log.
+
+git reset HEAD file		// reset can also be used to put files back to workspace from index. you can use HEAD to specify which version to reset.
 
 git reflog			// if the current terminal is closed, to find the commit ID, use this.
+
+index file in .git dir		// when we modify some file or add some new file, after "git add", the modification records are stored in the index file of .git directory. After "git commit", all the files in index are pushed to the master branch. Then index become "clean" again.
+
+git checkout -- file 		// when you want to cancel all the changes you made, reset the repo to teh version of last "add" or "commit". This can also be used when you want to recover the file you deleted locally from the repo.
+
+//delete file:
+
+rm file				// remove local files
+
+git rm file 			// remove repo files
+
+git checkout -- file		// recover the deleted file from repo to local
+
+//remote repo: Git uses ssh protocol for encryption. The private key and the public key are in ~/.ssh dir. .pub file is the public key. Githuub is the web providing remote repo service for Git.
+
+//here can add local git repo to a remote empty repo on Github. 
+
+git remote add origin git@github.com:wilsonli5000/reponame.git
+
+
 
 
