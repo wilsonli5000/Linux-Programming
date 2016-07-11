5615905f235faa44a3bd2c661b815ec267abec62
Wilson Li <WilsonLi@Wilsons-Macbook.local>
2016-07-11 11:33:22 -0700

    Add bash commands

--- bshell_cmd.sh
+++ bshell_cmd.sh
@@ -21,4 +21,25 @@ pushd <pathofdir>
 #pop back to teh previous working directory
 popd
 
+#walk through the file system tree from the current folder and execute commands.
+find . -exec [command lines] {} \;
+
+#measure the processing time of a process
+add time before the command line.
+time find . -exec ls
+
+#determine path as directory or file
+#first put the pwd to the current folder if the path is a relative path.
+if [[ -d $p ]]
+if [[ -f $p ]]
+
+#get the file name and extension from path
+filename="${p##*/}"     #"${p##patern} means trim the longest string with the patern from the begininng"
+fileextension="${p##.}"
+#"${p#patern} means trim the shortest string with the patern from the beginning"
+#"${p%%patern} means trim the longest string with the patern from the end"
+#"${p%patern} means trim the shortest string with the patern from the end"
+
+#sort the file which gathers the same lines together, then unique and count the number, then sort the result with reversed numerical order
+sort [file] | uniq -c | sort -nr
  
